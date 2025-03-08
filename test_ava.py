9bb8ed89e45976e37c1e744c95eb19d05c96f173
Viliuskerutis <32612736+Viliuskerutis@users.noreply.github.com>
2025-03-08 23:10:37 +0200

    test AVA IP updated

--- test_ava.py
+++ test_ava.py
@@ -1,7 +1,15 @@
 import requests
 
-url = "http://51.21.224.254:5000/evaluate"
-file_path = "C:/Users/kerut/OneDrive/Dokumentai/AVA/AVA/data/images/test/175e9250451079.58d10d89ef851.jpg"
+url = "http://51.21.218.121:5000/evaluate"
+# This one does not exists inside the storage of paintings
+#file_path = "data/images/test/175e9250451079.58d10d89ef851.jpg"
+#file_path = "data/images/test/images.jpg"
+#file_path = "data/images/test/007.jpg"
+file_path = "data/images/test/289.JPG"
+
+# This one exists inside the storage of paintings
+#file_path = "data/images/menorinka/Jonas-Mackevičius_Capri-salos-motyvas-su-žydinčia-visterija_48.jpg"
+#file_path = "data/images/menorinka/Solomonas-Teitelbaumas_Kelias-pro-žydintį-sodą_63.jpg"
 
 with open(file_path, "rb") as file:
     response = requests.post(url, files={"file": file})
